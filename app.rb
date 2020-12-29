0de0fa2565f6cf0f55fcca0b2f55569499d0bec5
Murodjon <murodjon.tursunpulatov@gmail.com>
2020-12-29 12:52:47 +0500

    added my_each_with_index method

--- app.rb
+++ app.rb
@@ -7,13 +7,35 @@ module Enumerable
       self
     end
 
+    def my_each_with_index(&prc)
+      self.length.times do |i|
+        prc.call(self[i],i)
+      end
+      self
+    end
+
     num_arr = [1, 2, 3]
     
-    puts "each ---"
-    p num_arr.my_each { |n| n }
-    num_arr.my_each do |n|
+  puts "--- my_each ---"
+  p num_arr.my_each { |n| n }
+  num_arr.my_each do |n|
       puts n*2 
-    end
-    puts "\n"
+  end
+  puts "\n"
+
+  puts "--- my_each_with_index ---"
+  p num_arr.my_each_with_index { |n,ind| "#{n} is #{ind}" }
+  num_arr.my_each_with_index do |n,ind|
+    puts "#{n} is #{ind}"
+  end
+
+  puts "\n"
+
+  hash = Hash.new
+  %w(cat dog wombat).my_each_with_index { |item, index|
+  hash[item] = index
+  }
+  p hash 
+  puts "\n"
 
   end
