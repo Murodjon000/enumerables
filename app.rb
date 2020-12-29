dbd882cbc6c61850d959e115ae0b2cb39c8abb69
Murodjon <murodjon.tursunpulatov@gmail.com>
2020-12-29 14:32:53 +0500

    added my_all method

--- app.rb
+++ app.rb
@@ -25,9 +25,22 @@ module Enumerable
     selected
   end
 
+
+  def my_all?(arg = nil, &prc)
+    if arg
+      my_each { |ele| return false unless arg === ele } 
+    elsif block_given?
+      my_each { |ele| return false unless yield(ele) }
+    else
+      my_each { |ele| return false unless ele }
+    end
+    true
+  end
+
+
     
   puts "--- my_each ---"
-  [1, 2, 3].my_each {|num| p num*2}
+  %w[Sharon Leo Leila Brian Arun].my_each { |friend| puts friend }
   puts "\n"
 
   puts "--- my_each_with_index ---"
@@ -40,8 +53,16 @@ module Enumerable
   puts "--- my_select ---"
   p [1,2,3,4,5].my_select { |num|  num.even?  }   #=> [2, 4]
 
+  puts "\n"
+  puts "--- my_all ---"
 
+  p %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+  p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+  p %w[ant bear cat].my_all?(/t/)                        #=> false
+  p [1, 2i, 3.14].my_all?(Numeric)                       #=> true
+  p [nil, true, 99].my_all?                              #=> false
+  p [].all?                                              #=> true
 
-
+  
 
   end
